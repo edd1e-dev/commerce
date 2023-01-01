26ac033891171ae0aa25983d7b02fc7a274b0694
Eddie <20160658@gm.hannam.ac.kr>
2023-01-01 16:24:51 +0900

    Init: "Spring 프로젝트 셋업" 프로젝트 기본 구조를 셋업하였습니다. Resolves: Ref: Related to:

---
+++ src/main/java/com/edd1e/commerce/CommerceApplication.java
@@ -0,0 +1,13 @@
+package com.edd1e.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceApplication.class, args);
+    }
+
+}
